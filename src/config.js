3df36002480ae3d3ae63f4acaf645e076a6f24d1
krogank9 <wifi.mouse.app@gmail.com>
2019-11-25 21:36:13 -0500

    change endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://boiling-chamber-32694.herokuapp.com/',//'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }

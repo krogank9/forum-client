7975d6935c6abf3ac6fb2a0d9324010f304a4edf
krogank9 <wifi.mouse.app@gmail.com>
2019-11-26 09:35:30 -0500

    make endpoint dynamic

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  //API_ENDPOINT: 'http://localhost:8000/api',//
-  API_ENDPOINT: 'https://boiling-chamber-32694.herokuapp.com/api',
+  //API_ENDPOINT: 'http://localhost:8000/api',
+  //API_ENDPOINT: 'https://boiling-chamber-32694.herokuapp.com/api',
+  API_ENDPOINT: window. location.hostname === "localhost"? 'http://localhost:8000/api' : 'https://boiling-chamber-32694.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }

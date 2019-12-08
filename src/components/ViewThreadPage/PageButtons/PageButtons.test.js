21868c4cb634b2a70b21f16256138268a0e400bb
krogank9 <wifi.mouse.app@gmail.com>
2019-12-08 18:12:24 -0500

    remove unneeded component

--- src/components/ViewThreadPage/PageButtons/PageButtons.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageButtons from './PageButtons';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PageButtons />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});

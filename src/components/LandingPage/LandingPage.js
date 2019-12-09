7380e6e95b7cdf6b2ccf476c31ff4042b2ceda20
krogank9 <wifi.mouse.app@gmail.com>
2019-12-08 20:57:16 -0500

    add links to client/server repos on landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ class LandingPage extends Component {
         <h1>Welcome to my forum</h1>
         <p>Click the "View boards" button in the navigation bar above to start browsing the various categories and topics.</p>
         <p>To login to a demo account, use the username "test" with the password "abc123".</p>
-        <p>Frontend: React. Backend: Node, ExpressJS, PostgreSQL.</p>
+        <p><a href="https://github.com/krogank9/forum-client">Client</a>: React, HTML, CSS. <a href="https://github.com/krogank9/forum-server">Server</a>: Node, ExpressJS, PostgreSQL.</p>
         <p>Created by <a href="http://ltkdigital.com">Logan Krumbhaar</a>.</p>
       </div>
     );
